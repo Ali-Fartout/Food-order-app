1c5465653fe8f6738e79e549054bbd53d6626ba7
Ali Fartout <alifa2080@gmail.com>
2021-11-21 19:49:09 +0330

    TODO: Add pre-purchase menu,Add Adding items

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = ()=>{
+    return(
+        <section className={classes.summary}>
+            <h2>The food ,Delivered to you</h2>
+            <p>Choose your favorite food</p>
+        </section>
+    );
+
+};
+export default MealsSummary;
